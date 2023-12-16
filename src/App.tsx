d7216dfbaa22aba12fba5d9a9569fde7fdb45d7c
yonghk423 <yonghk423423@gmail.com>
2023-12-16 20:17:22 +0900

    styled components global style

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import Home from "./routes/home";
 import Profile from "./routes/profile";
 import Login from "./routes/login";
 import CreateAccount from "./routes/create-account";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -28,10 +31,22 @@ const router = createBrowserRouter([
     ],
   },
 ]);
+const GlobalStyles = createGlobalStyle`
+    ${reset};
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    background-color: black;
+    color:white;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
 
 function App() {
   return (
     <>
+      <GlobalStyles />
       <RouterProvider router={router} />
     </>
   );
