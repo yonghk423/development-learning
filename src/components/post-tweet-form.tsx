8b36f90dabfbf4405737dd718818cc9b4109519a
yonghee <yonghee@yongheeui-MacBookPro.local>
2024-02-04 00:16:33 +0900

    Uploading Images

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,7 +1,8 @@
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, updateDoc } from "firebase/firestore";
 import { useState } from "react";
 import { styled } from "styled-components";
-import { auth, db } from "../firebase";
+import { auth, db, storage } from "../firebase";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 export default function PostTweetForm() {
     const [isLoading, setLoading] = useState(false);
@@ -26,12 +27,25 @@ export default function PostTweetForm() {
 
         try {
             setLoading(true);
-            await addDoc(collection(db, "tweets"), {
+            const doc = await addDoc(collection(db, "tweets"), {
                 tweet,
                 createdAt: Date.now(),
                 username: user?.displayName || "Anonymous",
                 userId: user?.uid,
             })
+            if (file) {
+                const locationRef = ref(
+                    storage,
+                    `tweets/${user?.uid}-${user?.displayName}/${doc.id}`
+                );
+                const result = await uploadBytes(locationRef, file);
+                const url = await getDownloadURL(result.ref);
+                await updateDoc(doc, {
+                    photo: url,
+                });
+            }
+            setTweet("");
+            setFile(null);
         } catch (e) {
             console.log(e);
         } finally {
@@ -41,6 +55,7 @@ export default function PostTweetForm() {
     return (
         <Form onSubmit={onSubmit}>
             <TextArea
+                required
                 rows={5}
                 maxLength={100}
                 onChange={onChange}
